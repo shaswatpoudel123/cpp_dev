a1ec8fea457ae74c7d53e83db6e939043c0728e0
shaswatpoudel123 <shaswatpoudel100@gmail.com>
2025-02-11 12:16:08 +0545

    pointer done

---
+++ pointter.cpp
@@ -0,0 +1,33 @@
+/*4. Create a class student having private members marks, name and public members rollno,
+getdata(), printdata(). Demonstrate concept of pointer to class members for array of 3
+objects.*/
+#include<iostream>
+#include<string>
+using namespace std;
+class student{
+    private:
+    int marks;
+    string name;
+    public:
+    int rollno;
+    void getdata(){
+        cout<<"Enter the name"<<endl;
+        getline(cin,name);
+        cout<<"Enter the roll no"<<endl;
+        cin>>rollno;
+        cout<<"Enter the marks"<<endl;
+        cin>>marks;
+}
+ void showdata(){
+    cout<<"The name is "<<name;
+    cout<<"The rollno is "<<rollno;
+    cout<<"The marsks is "<<marks;
+}
+};
+int main(){
+    student S;
+    S.getdata();
+    student *s=&S;
+    s->showdata();
+    return 0;
+}
