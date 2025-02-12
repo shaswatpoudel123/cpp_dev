683d6f7a121ee6671edf083878645011d4774c9d
shaswatpoudel123 <shaswatpoudel100@gmail.com>
2025-02-12 08:22:22 +0545

    done

--- class_basic_typeconversion.cpp
+++ class_basic_typeconversion.cpp
@@ -1,2 +1,23 @@
 #include<iostream>
 using namespace std;
+class sample{
+    private:
+    float a;
+    public:
+    sample(){
+        a=10.54;
+    }
+    //using a casting operator function 
+    operator int(){
+        int x;
+        x=a;
+        return x;
+    } 
+};
+int main(){
+    sample s;
+    int y=s;
+    cout<<"the value of x is "<<y<<endl;
+    return 0;
+}
+
